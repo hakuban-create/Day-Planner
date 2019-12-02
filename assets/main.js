1f516c5acb0a1065e4be25cad26022ee5d48f83a
Khaliunaa Baasandorj <khaliunaabaasandorj@Khaliunaas-MacBook-Pro-2.local>
2019-12-01 21:43:52 -0500

    updated to save in localstorage

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 var currentTime=moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#current-time").html(currentTime);
 
-//displayEntries();
+displayEntries();
 setColors();
 
 
@@ -17,6 +17,17 @@ setInterval(function(){
     }
     
 },1000);
+
+function displayEntries(){
+    for(var i=0; i<localStorage.length; i++){
+        if(localStorage.key(i).startsWith("dayPlanner")){
+            var key=localStorage.key(i);
+            var index=parseInt(key.slice(key.indexOf("_")+1));
+            console.log("index: "+index);
+            $("textarea").eq(index).text(localStorage.getItem(key));
+    }
+    }
+}
 
 function setColors(){
     var currentTime=moment().hour();
@@ -36,7 +47,7 @@ function setColors(){
 $("button").on('click',function(){
 var time=$(this).parent().parent().children().attr("id");
 var value=$(this).parent().parent().children().val();
-localStorage.setItem("Day_Planner_"+time,value);
+localStorage.setItem("dayPlanner_"+time,value);
 });
 
 
